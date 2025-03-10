f9785c28464da6ad5fc4a32e5fbf829599d206f6
kevin <kevinsanchezduran4@gmail.com>
2025-03-10 10:22:04 -0600

    Agregar archivos de proyecto.

---
+++ Practicadata/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practicadata.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
