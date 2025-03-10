021ea1004d04f89be46e2db9e5122029268ae2f9
kevin <kevinsanchezduran4@gmail.com>
2025-03-10 11:05:25 -0600

    insertar controlador automatico

---
+++ Practicadata/Models/Proyecto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practicadata.Models;
+
+public partial class Proyecto
+{
+    public int Id { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string? Descripcion { get; set; }
+}
--- Practicadata/Program.cs
+++ Practicadata/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Practicadata.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<CatalogosDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+});
 
 var app = builder.Build();
 
