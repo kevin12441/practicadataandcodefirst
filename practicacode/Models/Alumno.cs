55aab869b290fadb03094cb2889189a23e3c9b39
kevin <kevinsanchezduran4@gmail.com>
2025-03-10 21:27:22 -0600

    alumno

---
+++ practicacode/Models/Alumno.cs
@@ -0,0 +1,13 @@
+﻿namespace practicacode.Models
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Codigo { get; set; }
+        public string Direccion { get; set; }
+        public string Email { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public short YearEstudio { get; set; }
+    }
+}
---
+++ practicacode/Models/AlumnoDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace practicacode.Models
+{
+    public class AlumnoDbContext : DbContext
+    {
+        public AlumnoDbContext(DbContextOptions<AlumnoDbContext> options) : base(options) { }
+        public DbSet<Alumno> Alumno { get; set; }
+
+    }
+}
