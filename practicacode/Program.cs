7b9ecda9247d8a3c2133ddc62ba0c9b4d496a44d
kevin <kevinsanchezduran4@gmail.com>
2025-03-10 20:50:48 -0600

    Add-Migration Inicial

--- practicacode/Program.cs
+++ practicacode/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+using practicacode.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<EmpleadoDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+});
+    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
